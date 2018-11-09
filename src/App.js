a8782cd3ef01a5ff88ba574d882a85c337d9f0e8
tdw78 <tdweiss1978@gmail.com>
2018-11-09 11:33:16 -0800

    To-Do Props and State Checkpoint Complete

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  constructor(props) {
+     super(props);
+     this.state = {
+       todos: [
+         { description: 'Walk the cat', isCompleted: true },
+         { description: 'Throw the dishes away', isCompleted: false },
+         { description: 'Buy new dishes', isCompleted: false }
+       ]
+     };
+   }
+
   render() {
     return (
       <div className="App">
-      <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <ul>
+         { this.state.todos.map( (todo, index) =>
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+          )}
+       </ul>
       </div>
     );
   }
